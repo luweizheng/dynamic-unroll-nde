8c8aba4483d94ee254a02d66cb851543439cedb9
luweizheng <luweizheng36@hotmail.com>
2022-08-15 14:08:16 +0800

    unroll test

--- unroll_test/eqx_unroll.py
+++ unroll_test/eqx_unroll.py
@@ -10,7 +10,7 @@ import equinox as eqx
 from jax.config import config
 # We use GPU as the default backend.
 # If you want to use cpu as backend, uncomment the following line.
-config.update("jax_platform_name", "cpu")
+# config.update("jax_platform_name", "cpu")
 
 class MLP(eqx.Module):
     dense1: eqx.nn.Linear
@@ -36,18 +36,24 @@ class Con(eqx.Module):
     """
     """
     conv1: eqx.nn.Conv
+    conv2: eqx.nn.Conv
+    conv3: eqx.nn.Conv
     relu: jnn.relu
 
     def __init__(self):
         super(Con, self).__init__()
-        self.conv1 = eqx.nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, padding=1, key=jrandom.PRNGKey(0))
+        self.conv1 = eqx.nn.Conv2d(in_channels=16, out_channels=256, kernel_size=3, padding=1, key=jrandom.PRNGKey(0))
+        self.conv2 = eqx.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1, key=jrandom.PRNGKey(0))
+        self.conv3 = eqx.nn.Conv2d(in_channels=256, out_channels=16, kernel_size=3, padding=1, key=jrandom.PRNGKey(0))
         self.relu = jnn.relu
 
     def __call__(self, x):
         # 使用定义好的运算构建前向网络
         x = self.conv1(x)
         x = self.relu(x)
-        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.conv3(x)
         x = self.relu(x)
         return x
 
@@ -58,7 +64,7 @@ class Unroll(eqx.Module):
     def __init__(self):
         super(Unroll, self).__init__()
         self.mlp = Con()
-        self.unroll = 1
+        self.unroll = 20
     
     def __call__(self, x, num_steps):
         def step_fn(x, input=None):
@@ -73,7 +79,7 @@ class Unroll(eqx.Module):
 
 x = jnp.ones(shape=(128, 16, 224, 224))
 model = Unroll()
-num_steps = 50
+num_steps = 100
 
 @eqx.filter_jit
 def forward_fn(x):
